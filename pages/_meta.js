5d2fc570bd1a417de357c8e2f40cbaac53252dc2
Mathis Makarski <mathis.makarski@menschcologne.com>
2024-11-10 06:35:28 +0000

    initial commit

---
+++ pages/_meta.js
@@ -0,0 +1,13 @@
+export default{
+  "index": "Introduction",
+  "another": "Another Page",
+  "advanced": "Advanced (A Folder)",
+  "about": {
+    "title": "About",
+    "type": "page"
+  },
+  "archive": {
+    "title": "Archive",
+    "type": "page"
+  },
+}
